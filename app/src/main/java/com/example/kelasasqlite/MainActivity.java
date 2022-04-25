50c61ec8a6620b7e13d68a02b4fb724b656601e0
AnditaKhoiriah <andita.khoiriah.ft20@mail.umy.ac.id>
2022-04-26 01:10:36 +0700

    initial add 1

--- app/src/main/java/com/example/kelasasqlite/MainActivity.java
+++ app/src/main/java/com/example/kelasasqlite/MainActivity.java
@@ -2,27 +2,46 @@ package com.example.kelasasqlite;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.kelasasqlite.adapter.TemanAdapter;
 import com.example.kelasasqlite.database.DBController;
 import com.example.kelasasqlite.database.Teman;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
     private RecyclerView recyclerView;
     private TemanAdapter adapter;
-    private ArrayList<Teman> temanArrayList;
+    private ArrayList<Teman> temanArrayList = new ArrayList<>();
     DBController controller = new DBController(this);
     String id,nmn,tlp;
-    private FloatingActionButton fab;
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static String url_select = "http//10.0.2.2/umyTI/bacateman.php";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_TELPON = "telepon";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,23 +58,42 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,TemanBaru.class);
+                Intent intent = new Intent(MainActivity.this,TambahTeman.class);
                 startActivity(intent);
             }
         });
 
     }
     public void BacaData(){
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
 
-        ArrayList<HashMap<String,String>> dafterTeman = controller.getAllTeman();
-        temanArrayList = new ArrayList<>();
-        for (int i = 0; i<dafterTeman.size(); i++){
-            Teman teman = new Teman();
-            teman.setId(dafterTeman.get(i).get("id").toString());
-            teman.setNama(dafterTeman.get(i).get("nama").toString());
-            teman.setTelpon(dafterTeman.get(i).get("telpon").toString());
+        JsonArrayRequest jArr = new JsonArrayRequest(url_select, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, response.toString());
 
-            temanArrayList.add(teman);
-        }
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject obj = response.getJSONObject(i);
+                        Teman item = new Teman();
+                        item.setId(obj.getString(TAG_ID));
+                        item.setNama(obj.getString(TAG_NAMA));
+                        item.setTelpon(obj.getString(TAG_TELPON));
+                        temanArrayList.add(item);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG,"Error: "+error.getMessage());
+                error.printStackTrace();
+                Toast.makeText(MainActivity.this,"gagal",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        requestQueue.add(jArr);
     }
 }
